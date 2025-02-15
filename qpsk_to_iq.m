cb98200f8d951a103cd5095dd2119c2a186e364f
shuchu11 <163873199+shuchu11@users.noreply.github.com>
2025-02-15 19:54:38 +0800

    Add files via upload

---
+++ qpsk_to_iq.m
@@ -0,0 +1,18 @@
+function iq_values = qpsk_to_iq(qpsk_symbols)
+    % QPSK 對應的 IQ 值 (Gray 編碼)
+    qpsk_map = containers.Map({0, 1, 2, 3}, ...
+                              {1+1j, -1+1j, -1-1j, 1-1j});
+    
+    % 初始化 IQ 值陣列
+    iq_values = zeros(size(qpsk_symbols));
+    
+    % 轉換 QPSK 符號至 IQ 值
+    for k = 1:length(qpsk_symbols)
+        if isKey(qpsk_map, qpsk_symbols(k))
+            iq_values(k) = qpsk_map(qpsk_symbols(k));
+        else
+            error('輸入的 QPSK 符號必須是 0, 1, 2, 或 3');
+        end
+    end
+end
+
